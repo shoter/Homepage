df741a5904d2eda6e4af2ca20226f21ac1961301
shoter <lubiebardzozelki@gmail.com>
2019-05-20 01:03:42 +0200

    Test

--- Deploy/Tests.ps1
+++ Deploy/Tests.ps1
@@ -4,16 +4,21 @@ Write-Output (Get-Location)
 
 echo "Starting tests for react"
 # Test react solution
+$env:CI = "true"
 yarn
-($env:CI = "true") -and (yarn test)
+yarn run test
+
+if($? -eq $False) {
+    exit 1
+}
 
 Set-Location ../Deploy
-
 
 Set-Location FtpDeployCore.Tests
 
 Write-Output (Get-Location)
 echo "Starting tests for C#"
+echo $result
 
 dotnet test
 
