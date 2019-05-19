4a44c673d0b41c1bac9a42ee087963beffd6a554
shoter <lubiebardzozelki@gmail.com>
2019-05-20 00:10:34 +0200

    test

--- Deploy/Tests.ps1
+++ Deploy/Tests.ps1
@@ -1,10 +1,17 @@
 Set-Location app
 
+echo Get-Location
+echo "Starting tests for react"
 # Test react solution
 ($env:CI = "true") -and (yarn test)
 
 Set-Location ../Deploy
 
+
 Set-Location FtpDeployCore.Tests
+
+echo Get-Location
+echo "Starting tests for C#"
+
 dotnet test
 
