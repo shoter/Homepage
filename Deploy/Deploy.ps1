61708f6372f2655d7441bb1f51309ae0384cee68
shoter <lubiebardzozelki@gmail.com>
2019-05-20 23:44:43 +0200

    Changed deploy script

--- Deploy/Deploy.ps1
+++ Deploy/Deploy.ps1
@@ -10,6 +10,7 @@ dotnet build
 #Goto build location
 Set-Location bin\Debug\netcoreapp2.2
 
-Write-Output "User - " . $ENV:FTP_USR
+Write-Output "User - $($ENV:FTP_USR)"
+$Path = Resolve-Path "..\..\..\..\..\app\build\"
 #Transfer Files
-dotnet .\FtpDeployCore.dll "..\..\..\..\..\app\build\" $ENV:FTP_USR $ENV:FTP_PSW
+dotnet .\FtpDeployCore.dll $Path $ENV:FTP_USR $ENV:FTP_PSW
