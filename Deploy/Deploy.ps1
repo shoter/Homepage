056b911ab11f5bc8b37b793f23c8993111c079cd
shoter <lubiebardzozelki@gmail.com>
2019-05-20 23:08:11 +0200

    Cleaned deploy file

--- Deploy/Deploy.ps1
+++ Deploy/Deploy.ps1
@@ -1,17 +1,8 @@
 Set-Location app
 
-# Test react solution
-($env:CI = "true") -and (yarn test)
-
 yarn build
 
 Set-Location ../Deploy
-
-Set-Location FtpDeployCore.Tests
-dotnet test
-Set-Location ..
-
-# Build FtpDeployCore
 
 Set-Location FtpDeployCore
 dotnet build
