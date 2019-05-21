77abfab1871f86c6f1b54d51209ecc296542951e
shoter <lubiebardzozelki@gmail.com>
2019-05-22 01:43:36 +0200

    Changed how deploy works

--- Deploy/Deploy.ps1
+++ Deploy/Deploy.ps1
@@ -1,16 +1,15 @@
 Set-Location app
 
-yarn build
+#yarn build
 
 Set-Location ../Deploy
 
 Set-Location FtpDeployCore
-dotnet build
+#dotnet build
 
 #Goto build location
 Set-Location bin\Debug\netcoreapp2.2
 
 $Path = Resolve-Path "..\..\..\..\..\app\build"
-
 #Transfer Files
 dotnet .\FtpDeployCore.dll "path=$($Path)" "login=$($ENV:FTP_USR)" "pass=$($ENV:FTP_PSW)"
