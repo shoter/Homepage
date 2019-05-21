ffe95e336dc8b2151dcdb4167472166acee0428b
shoter <lubiebardzozelki@gmail.com>
2019-05-22 00:23:26 +0200

    Wrong folder

--- Deploy/Deploy.ps1
+++ Deploy/Deploy.ps1
@@ -10,7 +10,7 @@ dotnet build
 #Goto build location
 Set-Location bin\Debug\netcoreapp2.2
 
-$Path = Resolve-Path "..\..\..\..\..\app\build"
+$Path = Resolve-Path "..\..\..\..\..\app\build\"
 Write-Output "User - $($ENV:FTP_USR)"
 Write-Output "Path - $($Path)"
 #Transfer Files
