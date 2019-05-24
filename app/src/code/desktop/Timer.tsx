a454e78fe68088ba6b7e4cdd34010a7bd2bfc714
shoter <lubiebardzozelki@gmail.com>
2019-05-24 11:58:28 +0200

    Fiex timer

--- app/src/code/desktop/Timer.tsx
+++ app/src/code/desktop/Timer.tsx
@@ -1,39 +1,45 @@
 import React, { Component } from "react";
 
 interface TimerState {
-    time: Date,
-    intervalTimeout?: NodeJS.Timeout
+  time: Date;
+  intervalTimeout?: NodeJS.Timeout;
 }
 
-export default class Timer extends Component<{}, TimerState>
-{
-    constructor(props : {} ) {
-        super(props)
+export default class Timer extends Component<{}, TimerState> {
+  constructor(props: {}) {
+    super(props);
 
-        this.state = {
-            time:new Date()
-        }
-    }
+    this.state = {
+      time: new Date()
+    };
+  }
 
-    setNewTime = () => {
-        this.setState({
-            time: new Date()
-        })
-    }
+  setNewTime = () => {
+    this.setState({
+      time: new Date()
+    });
+  };
 
-    componentDidMount() {
-        const intervalTimeout = setInterval(this.setNewTime, 1000);
-        this.setState({intervalTimeout: intervalTimeout});
-    }
+  componentDidMount() {
+    const intervalTimeout = setInterval(this.setNewTime, 1000);
+    this.setState({ intervalTimeout: intervalTimeout });
+  }
 
-    componentWillDisMount() {
-        if(this.state.intervalTimeout)
-            clearInterval(this.state.intervalTimeout);
-    }
-    render() {
-        const {time} = this.state;
-        return (<div className="gl-timer">
-            {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
-        </div>)
-    }
+  componentWillDisMount() {
+    if (this.state.intervalTimeout) clearInterval(this.state.intervalTimeout);
+  }
+
+  formatTimePart = (number: number) => {
+    return ("0" + number).slice(-2);
+  };
+
+  render() {
+    const { time } = this.state;
+    const f = this.formatTimePart;
+    return (
+      <div className="gl-timer">
+        {f(time.getHours())}:{f(time.getMinutes())}
+      </div>
+    );
+  }
 }
