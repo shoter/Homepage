6c74ebedc69a7fb1b6566c79ab0088a261b03ab9
shoter <lubiebardzozelki@gmail.com>
2019-08-06 01:01:13 +0200

    Used react-ga lib to add analytics

--- app/src/code/external/GoogleAnalytics.tsx
+++ app/src/code/external/GoogleAnalytics.tsx
@@ -1,17 +1,6 @@
-import React, {Component} from "react";
-
-declare var dataLayer : any;
+import ReactGA from 'react-ga';
 
 export function loadAnalytics() {
-    // thanks to https://stackoverflow.com/questions/8578617/inject-a-script-tag-with-remote-src-and-wait-for-it-to-execute
-    let script = document.createElement("script") as HTMLScriptElement;
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = "https://www.googletagmanager.com/gtag/js?id=UA-106497042-1"
-    script.onload = function() {
-        dataLayer = dataLayer || [];
-        dataLayer.push(['js', new Date()]);
-        dataLayer.push(['config', 'UA-106497042-1']);
-    }
-    document.getElementsByTagName('head')[0].appendChild(script);
+    ReactGA.initialize('UA-106497042-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
 }
