5fdadbc9595c185607969c6e62714d14c58e055e
Damian Laczak <shoter@users.noreply.github.com>
2023-12-10 04:10:57 +0100

    https

--- app/src/code/Windows/Cv.tsx
+++ app/src/code/Windows/Cv.tsx
@@ -5,7 +5,7 @@ export default class CV extends Component
 
     render()
     {
-        return <iframe width="100%" height="100%" src="http://damian.laczak.net.pl/resume.pdf" />
+        return <iframe width="100%" height="100%" src="https://damian.laczak.net.pl/resume.pdf" />
     }
 
 }
